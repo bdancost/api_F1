bc7e2fadd1fb77bc8b002b9a7dd23509118736dd
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-01-21 21:27:11 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { drivers } from "./data.js";
+
+const baseAPIRoute = "/api/v1";
+
+const app = express();
+
+app.get(baseAPIRoute + "/drivers", (req, res) => {
+  res.status(200).send(drivers);
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(` Servidor rodando na porta ${port}`);
+});
